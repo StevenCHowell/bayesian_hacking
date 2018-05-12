faca24fc899b51bb89c59eb27cfe4c5fd339fd4e
Steven C. Howell <howell_steven@bah.com>
2018-05-12 00:53:58 -0400

    incorporate generator updates

--- coin.py
+++ coin.py
@@ -1,52 +1,113 @@
+import time
 import numpy as np
+import bokeh.plotting
+import bokeh.io
+import bokeh.layouts
+from bokeh.palettes import Category10_10 as palette
 
-p_a = 1.0 / 100.0
-p_ba = 1.0
-p_na = 99.0 / 100.0
-p_bna = 0.5 ** 10
-p_b = p_ba * p_a + p_bna * p_na
-p_ab = p_ba * p_a / p_b
 
+# p_a = 1.0 / 100.0
+# p_ba = 1.0
+# p_na = 99.0 / 100.0
+# p_bna = 0.5 ** 10
+# p_b = p_ba * p_a + p_bna * p_na
+# p_ab = p_ba * p_a / p_b
+
+
+def flip_generator(coins, *, n_flips=10):
+    n_coins = len(coins)
+    
+    coin_index = np.random.randint(0, n_coins, 1)[0]
+    drawn_coin = coins[coin_index]
+    coin_unfair = drawn_coin == 1  # did we get the unfair coin?
+
+    if coin_unfair:
+        all_heads = True
+    else: 
+        for j in range(n_flips):
+            flip = np.random.rand()  # random number between [0, 1)
+
+            if flip < 0.5:
+                all_heads = True
+            else:
+                all_heads = False
+                break
+
+    if all_heads:
+        # report the index and if the coin was unfair
+        yield coin_unfair
+
+
+def animate():
+    global callback_id
+    if button.label == '► Play':
+        button.label = '❚❚ Pause'
+        callback_id = bokeh.io.curdoc().add_periodic_callback(animate_update, 200)
+    else:
+        button.label = '► Play'
+        
+        bokeh.io.curdoc().remove_periodic_callback(callback_id)
+
+
+def animate_update():
+    global n_all_heads
+    global n_unfair
+    n_max = 100000 
+    n_show = 500
+
+    if n_all_heads > n_max:
+        bokeh.io.curdoct().remove_periodic_callback(callback_id)
+    
+    else:
+        new_data = dict(x=[0], y=[0])
+        res = list(flip_generator(coins))
+        if res:
+            n_all_heads += 1
+            if res[0]:
+                n_unfair += 1
+            frac_unfair = n_unfair / n_all_heads
+
+            new_data['x'] = [n_all_heads]  
+            new_data['y'] = [frac_unfair]  
+            
+            test_data.stream(new_data, n_show)
+
+            bokeh.io.push_notebook(handle=handle)
+
+            # time.sleep(slider.value)
+
+
+# 100 coin all fair
 n_coins = 100
-coins = np.zeros(n_coins, dtype=np.int)  # 100 coin all fair
-coins[np.random.randint(1, 100, 1)] = 1  # make one coin unfair
+coins = np.zeros(n_coins, dtype=np.int)  
 
-n_flips = 10
-n_samples = 1000000
+# make one coin unfair
+coins[np.random.randint(1, 100, 1)] = 1  
 
-all_heads_coins = []
+# generate the figure
+fig = bokeh.plotting.figure(plot_width=800, plot_height=400)
+test_data = bokeh.models.sources.ColumnDataSource(data=dict(x=[0], y=[0]))
+line = fig.line("x", "y", source=test_data)
 
-# v1
-for i in range(n_samples):
-    coin = coins[np.random.randint(1, 100, 1)[0]]
-    heads = 1
-    for j in range(n_flips):
-        flip = np.random.rand()  # random number between 0, 1
-        if coin == 1 or flip > 0.5:
-            heads = 1
-        else:
-            heads = 0
-            break
-    if heads:
-        all_heads_coins.append([i, coin])
+# set the globals
+callback_id = None
+n_all_heads = 0
+n_unfair = 0
 
-n_all_heads = len(all_heads_coins)
-all_heads_coins = np.array(all_heads_coins)
-all_heads_coins = np.c_[all_heads_coins, np.empty(n_all_heads)]
-for i in range(n_all_heads):
-    all_heads_coins[i, 2] = all_heads_coins[:i, 1].sum() * 1.0 / (i + 1)
-print('probability coin was unfair: {:.3}'.format(all_heads_coins[-1, 2]))
+# define the speed slider
+slider = bokeh.models.Slider(start=0, end=1, value=1, step=0.1, title="Speed")
+# slider.on_change('value', slider_update)
 
-try:
-    import bokeh.plotting
-    from bokeh.palettes import Category10_10 as palette
-    bokeh.plotting.output_file('coin.html')
+# define the play button
+button = bokeh.models.Button(label='► Play', width=60)
+button.on_click(animate)
 
-    p = bokeh.plotting.figure(
-        y_axis_label='draws when all heads was unfair coin',
-        x_axis_label='trials')
-    p.line(all_heads_coins[:, 0], all_heads_coins[:, 2], color=palette[0])
-    p.line(all_heads_coins[[0, -1], 0], [p_ab, p_ab], color=palette[1])
-    bokeh.plotting.show(p)
-except:
-    pass
+# layout the app
+layout = bokeh.layouts.layout([
+    [fig],
+    [slider, button],
+], sizing_mode='scale_width')
+bokeh.io.curdoc().add_root(layout)
+
+# execute the app
+bokeh.io.curdoc().title = "Coins"
